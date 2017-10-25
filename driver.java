c7044a3d7e3a6dea80fe6c7908ec4bc9e1104348
mirmulk <mirrahsinul-mulk@uiowa.edu>
2017-10-25 17:00:44 -0500

    audio

--- driver.java
+++ driver.java
@@ -1,8 +1,13 @@
+import javax.swing.*;
+
 public class driver
 {
     public static void main(String[] args)
     {
         int n = 0;
         System.out.printf("n=" + n);
+        JFrame frame = new Capture();
+        frame.pack();
+        frame.setVisible(true);
     }
 }
